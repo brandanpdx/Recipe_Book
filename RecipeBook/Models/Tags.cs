0c1185144e743817372b63d6516d3d4dd23ff0d7
Dusty <dustymccord@gmail.com>
2020-03-24 09:34:00 -0700

    Add Tag model.
    Co-authored-by: Dom <46074403+21-Coding@users.noreply.github.com>
    Co-authored by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

--- RecipeBook/Models/Tags.cs
+++ RecipeBook/Models/Tags.cs
@@ -2,5 +2,16 @@ using System.Collections.Generic;
 
 namespace RecipeBook.Models
 {
+  public class Tag
+  {
+    public Tag()
+    {
+      this.Recipes = new HashSet<RecipeTag>();
+    }
 
+    public int TagId { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<RecipeTag> Recipes { get; }
+  }
 }
