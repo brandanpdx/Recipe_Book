64f644ca019c773c6a240b65879e637fe7d96ed2
Dusty <dustymccord@gmail.com>
2020-03-24 09:59:36 -0700

    Add TagsController.
    Co-authored-by: Dom <46074403+21-Coding@users.noreply.github.com>
    Co-authored by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

---
+++ RecipeBook/Controllers/TagsControllers.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBook.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RecipeBook.Controllers
+{
+  public class TagsController : Controller
+  {
+    private readonly RecipeBookContext _db;
+
+    public TagsController(RecipeBookContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Tags.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CourseId = new SelectList(_db.Recipes, "RecipeId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(TagsController tag, int RecipeId)
+    {
+      _db.Tags.Add(tag);
+      if (RecipeId != 0)
+      {
+        _db.RecipeTag.Add(new RecipeTag() { RecipeId = RecipeId, TagId = tag.TagId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Tag thisTag = _db.Tags
+          .Include(tag => student.Recipes)
+          .ThenInclude(join => join.Recipe)
+          .FirstOrDefault(tag => tag.TagId == id);
+      return View(thisTag);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+      ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
+      return View(thisTag);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Tag tag, int RecipeId)
+    {
+      if (RecipeId != 0)
+      {
+        _db.RecipeTag.Add(new RecipeTag() { RecipeId = RecipeId, TagId = tag.TagId });
+      }
+      _db.Entry(tag).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddRecipe(int id)
+    {
+      Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+      ViewBag.CourseId = new SelectList(_db.Recipes, "RecipeId", "Name");
+      return View(thisTag);
+    }
+
+    [HttpPost]
+    public ActionResult AddRecipe(Tag tag, int RecipeId)
+    {
+      if (RecipeId != 0)
+      {
+        _db.RecipeTag.Add(new RecipeTag() { RecipeId = RecipeId, TagId = tag.TagId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+      return View(thisTag);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+      _db.Tags.Remove(thisTag);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
